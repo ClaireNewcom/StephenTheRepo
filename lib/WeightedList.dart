3a7c8890234516ac611433118d66f5dc9972d6d2
NickEck <neck@wpi.edu>
2024-04-03 17:12:01 +0000

    Redid what was before

--- lib/WeightedList.dart
+++ lib/WeightedList.dart
@@ -4,6 +4,42 @@ import 'package:flutter/material.dart';
 
 class WeightedList {
 
-  
+  late String clothing;
+  late int counter;
+
+  WeightedList(){
+    clothing = "";
+    counter = 0;
+  }
+
+  WeightedList.addElement(String str, int count){
+    clothing = str;
+    counter = count;
+  }
+ 
+  WeightedList.addElement(){
+    clothing = "";
+    counter = 0;
+  }
+
+  String getClothes() {
+    return clothing;
+  }
+
+  void setClothes(String newClothing) {
+    clothing = newClothing;
+  }
+
+  int getCounter() {
+    return counter;
+  }
+
+  void setCounter(int i){
+    counter = i;
+  }
+
+  void incrementCounter(){
+    counter++;
+  }
 
 }
