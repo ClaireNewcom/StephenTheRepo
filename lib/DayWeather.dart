af049d3c0964539ce04631993f21a54d61525cfc
NickEck <neck@wpi.edu>
2024-04-05 18:04:59 +0000

    read api class created

--- lib/DayWeather.dart
+++ lib/DayWeather.dart
@@ -2,25 +2,15 @@
 import 'dart:math';
 
 class Clothes {
-    //this class reads from the api, find the different aspects of the weiather, puts the information into a WeatherConditions type, and puts them in a list
-
+   //this class reads from the api, find the different aspects of the weiather, puts the information into a WeatherConditions type, and puts them in a list
+   int h = ReadApi.getTime();
   WeatherConditions nowWeath = new WeatherConditions()
   ArrayList<WeatherConditions> WeatherList = new ArrayList<WeatherConditions>();
     for(int i = 0; i<3 ;i++){ //the i's are for the different periods of the day
-    // this part is wrong, will make seperate class tomorow for reading things
-    int readTemp(){ 
-       //reads temp from the api, assign to t
-    }
-    int readWind(){
-       //reads wind from api, assign to w
-    }
-    int readHumid(){
-       //reads humidity from api, assign to h
-    }
-    int readType(){
-       //reads weather type from api, assign to e
-    }
-    nowWeath.setTemp(t);//do for all 4
+    nowWeath.setTemp(ReadApi.readTemp(4*i+h));//gets temperature based on hour + time away
+    nowWeath.setWind(ReadApi.readWind(4*i+h));
+    nowWeath.setHumid(ReadApi.readHumid(4*i+h));
+    nowWeath.setType(ReadApi.readType(4*i+h));
     WeatherList.add(nowWeath);
     nowWeath.clear();
     }
