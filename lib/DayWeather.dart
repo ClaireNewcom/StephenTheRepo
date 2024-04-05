ff3146fb1c87bbdbb57082cb3606537eeaedfd2a
NickEck <neck@wpi.edu>
2024-04-05 18:06:14 +0000

    whatup

--- lib/DayWeather.dart
+++ lib/DayWeather.dart
@@ -1,7 +1,7 @@
 //this class makes an array list of weather conditions for the day that will lter be sorted through by the WWTW algorithm
 import 'dart:math';
 
-class Clothes {
+class DayWeather {
    //this class reads from the api, find the different aspects of the weiather, puts the information into a WeatherConditions type, and puts them in a list
    int h = ReadApi.getTime();
   WeatherConditions nowWeath = new WeatherConditions()
