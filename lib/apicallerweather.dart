242250d5c7150ecac961983c1601026fc21075a8
ClaireNewcom <cnewcom@wpi.edu>
2024-04-08 08:56:12 -0400

    Api commit 1

---
+++ lib/apicallerweather.dart
@@ -0,0 +1,48 @@
+import "package:http/http.dart" as http;
+import 'dart:convert';
+
+//Some of this was learned from https://www.youtube.com/watch?v=jpLa3NjWqs0, some of the code was copied from here.
+//Some of this was learned from https://www.youtube.com/watch?v=c9XyI8zM73k, some of the code was copied from here.
+
+  const String baseURL = '//api.weather.gov/';
+
+  class ACallW {
+    var client = http.Client();
+    String? userAgent;
+
+    Future<dynamic> get(String api) async{
+      var url = Uri.parse(baseURL + api);
+      var _headers = {
+        'Authorization': 'userAgent', //I think this is how this works
+        'api_key': 'claire.newcom.cn@gmail.com',
+      };
+      var response = await client.get(url, headers:_headers);
+      if (response.statusCode == 200){
+        return response.body;
+      }
+      else {
+        throw Exception("Can not get weather"); //catch exception in user interface
+      }
+    }
+
+    Future<dynamic> post(String api) async{
+      var url = Uri.parse(baseURL + api);
+      var _headers = {
+        'Authorization': 'userAgent', //I think this is how this works
+        'api_key': 'claire.newcom.cn@gmail.com',
+       // 'Content-Type': 'application/json',
+      };
+
+      var response = await client.post(url, headers: _headers);
+      if (response.statusCode == 201) {
+        return response.body;
+      } else {
+        //throw exception and catch it in UI
+      }
+    }
+
+    Future<dynamic> put(String api) async{}
+
+    Future<dynamic> delete(String api) async{}
+
+  }
--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,19 +2,35 @@
 import 'dart:math';
 
 class quiz {
-double temp2 = Random.nextInt(110)-10;
-double wind2 = Random.nextInt(80);
-double humid2 = Random.nextInt(100);
-double weatherNum = Random.nextInt(4);
-if(weatherNum==0){
-String weather2 = 'sunny';
-}else if(weatherNum==1){
-String weather2 = 'cloudy';
-}else if(weatherNum==2){
-String weather2 = 'hail';
-}else{
-String weather2 = 'rainy';
-}
- Clothes(temp2, wind2, humid2, weather2);
-    
+  var random = Random();
+  late double temp2;
+  late double wind2;
+  late double humid2;
+  late double weatherNum;
+  late String weather2;
+  quiz(){
+    temp2 = (random.nextInt(110)-10) as double;
+    wind2 = (random.nextInt(80)) as double;
+    humid2 = random.nextInt(100) as double;
+    weatherNum = random.nextInt(4) as double;
+
+    if (weatherNum == 0){
+      weather2 = 'sunny';
+    }
+    else if(weatherNum==1){
+      weather2 = 'cloudy';
+    }
+    else if(weatherNum==2){
+      weather2 = 'hail';
+    }
+    else {
+      weather2 = 'rainy';
+    }
+  }
+
+
+ Clothes(temp2, wind2, humid2, weather2){
+
+  }
+
 }
