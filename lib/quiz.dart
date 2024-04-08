c47c771c0aa7b9c0b644d0dc9d5f8d5ee6cc4461
NickEck <neck@wpi.edu>
2024-04-08 13:23:28 +0000

    quiz

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -2,12 +2,17 @@
 import 'dart:math';
 
 class quiz {
+  WeatherConditions fakeWeather = new WeatherConditions();
+  List<String>clothe = [];
+
+    quiz(){
     Random randy = new Random();
     double temp2 = (randy.nextInt(110)-10).toDouble();
     double wind2 = (randy.nextInt(80)).toDouble();
     double humid2 = (randy.nextInt(100)).toDouble();
     int weatherNum = randy.nextInt(4);
     String weather2 = '';
+
     if(weatherNum==0){
         weather2 = 'sunny';
     }else if(weatherNum==1){
@@ -18,16 +23,21 @@ class quiz {
         weather2 = 'rainy';
     }
 
-    WeatherConditions fakeWeather = new WeatherConditions(temp2, wind2, humid2, weather2);
-    List<String>clothe = Clothes(fakeWeather);
+    fakeWeather = [temp2, wind2, humid2, weather2];
+    clothe = Clothes(fakeWeather);
+    }
 
     WeatherConditions ask(){
         return fakeWeather;
     }
-    Boolean is(String cloth){
+    List<String> fakeWhatToWear(){
+        return clothe;
+    }
+    Boolean correct(String cloth){
         if(clothe.contains(cloth)){
             return true;
-        }else{}
+        }else{
             return false;
         }
     }
+}
